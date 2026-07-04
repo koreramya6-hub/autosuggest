db53168c6cc4af114e97339494006cc52e0dded4
koreramya6-hub <koreramya6@gmail.com>
2026-07-04 10:16:16 +0530

    Random User

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 const PORT = 3002;
 
-app.use(express.static("frontend"))
+app.use(express.static("frontend"));
 
 app.listen(PORT, function() {
     console.log("successfully running at http://localhost:"+PORT)
